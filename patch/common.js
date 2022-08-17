89552f722add4138c799c46857f0993676b4f862
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2022-08-18 03:11:14 +0900

    Update HTML

---
+++ patch/common.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+	// document.body.insertAdjacentHTML("beforeend", `
+	// 	<div id="overlay">
+	// 		<div id="menu">
+	// 			<p> Hello world! 
+	// 			Lorem ipsum
+	// 			Lorem ipsum
+	// 			Lorem ipsum
+	// 			Lorem ipsum
+	// 			Lorem ipsum
+	// 			Lorem ipsum
+	// 			</p>
+	// 		</div>
+	// 	</div>
+	// `);
+	// const overlay = document.getElementById("overlay");
+	// overlay.onclick = () => {
+	// 	overlay.style.display = "none";
+	// }
+	// const menu = document.getElementById("menu");
+	// for(const e of document.getElementsByTagName("ruby")){
+	// 	const rubies = JSON.parse(e.attributes["data-candidates"].value);
+	// 	e.onclick = () => {
+	// 		overlay.style.display = "flex";
+	// 	};
+	// }
+};
